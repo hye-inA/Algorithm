f36255bbfa52ba7f4a28ef233d0ac57049183a31
hyein Heo <128613248+hye-inA@users.noreply.github.com>
2025-04-07 19:35:55 +0900

    [level 1] Title: 특정 형질을 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
+++ 프로그래머스/1/301646. 특정 형질을 가지는 대장균 찾기/특정 형질을 가지는 대장균 찾기.sql
@@ -1,3 +1,3 @@
 select count(*) as COUNT
 from ECOLI_DATA
-where (GENOTYPE & 2) = 0 and ((GENOTYPE & 1) > 0 or (GENOTYPE & 4) > 0)
+where (GENOTYPE & 2) = 0 and ((GENOTYPE & 1) != 0 or (GENOTYPE & 4) != 0)
